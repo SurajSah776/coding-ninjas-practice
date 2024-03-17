8b243ad1d700b924712ade97b7aaeb9414354cb6
SurajSah776 <surajkumarsah2060@gmail.com>
2024-03-17 23:32:01 +0530

    Easy Problems

---
+++ Basic Problems/All_Divisors_of_Number.cpp
@@ -0,0 +1,27 @@
+/* Print all Divisors of a Number
+Problem statement
+Given an integer ‘N’, your task is to write a program that returns all the divisors of ‘N’ in ascending order.
+
+For example:
+'N' = 5.
+The divisors of 5 are 1, 5.
+
+Sample Input 1 :
+10
+Sample Output 1 :
+1 2 5 10
+*/
+
+#include <iostream>
+using namespace std;
+
+int *printDivisors(int n, int &size)
+{
+}
+
+// Driver Code
+int main()
+{
+
+    return 0;
+}
